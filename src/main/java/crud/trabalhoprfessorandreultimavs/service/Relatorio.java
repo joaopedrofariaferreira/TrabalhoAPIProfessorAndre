1c77e7837633abf4fa4b5dcf7718edf18055f333
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2024-11-06 10:24:18 -0300

    nova classe que sera desenvolvida, funcionalidade relatorio

---
+++ src/main/java/crud/trabalhoprfessorandreultimavs/service/Relatorio.java
@@ -0,0 +1,6 @@
+package crud.trabalhoprfessorandreultimavs.service;
+
+public class Relatorio {
+
+        //Consumir da classe agenda, utilizar o método get para gerar o relatorio e transofrmar em PDF
+}
